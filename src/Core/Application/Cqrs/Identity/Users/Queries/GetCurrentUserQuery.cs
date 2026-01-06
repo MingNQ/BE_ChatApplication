6deaf9b3c852ae890b3f86d78b72f31a8dcfc7ad
Ming <minhlaminh01@gmail.com>
2026-01-06 15:49:44 +0700

    feat: add get current user

---
+++ src/Core/Application/Cqrs/Identity/Users/Queries/GetCurrentUserQuery.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Persistence;
+using Application.Cqrs.Identity.Users.Specs;
+using Application.Dto.Persistence.Catalog.User;
+using Domain.Entities.Identity;
+using Mapster;
+using MediatR;
+using Shared.Constants;
+
+namespace Application.Cqrs.Identity.Users.Queries;
+
+public class GetCurrentUserQuery : IRequest<SortUserInfo>;
+
+public class GetCurrentUserQueryHandler(
+    IReadRepository<User> userRepository,
+    ICurrentUser currentUser)
+    : IRequestHandler<GetCurrentUserQuery, SortUserInfo>
+{
+    public async Task<SortUserInfo> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
+    {
+        var spec = new UserByIdSpec(currentUser.UserId);
+        var user = await userRepository.FirstOrDefaultAsync(spec, cancellationToken);
+
+        if (user == null)
+        {
+            throw new NotFoundException(MessageCommon.SetEntityNotFound(nameof(User), currentUser.UserId));
+        }
+
+        return user.Adapt<SortUserInfo>();
+    }
+}
---
+++ src/Presentation/Host/Controllers/Client/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Cqrs.Identity.Users.Queries;
+using Host.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Constants;
+
+namespace Host.Controllers.Client;
+
+[ControllerName("users")]
+[Tags("Client|User")]
+public class UserController : BaseClientAuthController
+{
+    [HttpGet("current-user")]
+    public async Task<IActionResult> GetCurrentUser()
+    {
+        var result = await Mediator.Send(new GetCurrentUserQuery());
+        return Ok(result, MessageCommon.GetDataSuccess);
+    }
+}
